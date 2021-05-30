804e1b8f74828e9c425f414fafbe76709873a888
Prashantbhure50 <prashantbhure50@gmail.com>
2021-05-30 16:39:56 +0530

    Add project files.

---
+++ Linq/Managment.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    class Managment
+    {
+        ArrayList outputList = new ArrayList();
+        public void RetriveTopThreeRecord(List<ProductReview> productReviewsList)
+        {
+            var highestRatedRows = (from rec in productReviewsList
+                                    orderby rec.rating descending
+                                    select rec).Take(3);
+            foreach (var row in highestRatedRows)
+            {
+                Console.WriteLine(row.ToString());
+                outputList.Add(row.ToString());
+            }
+
+        }
+        public void RetriveAllRecordRatingGreaterThanThree(List<ProductReview> productReviewsList)
+        {
+            var data = (from rec in productReviewsList
+                        where rec.rating > 3 && (rec.productId == 1 || rec.productId == 3 || rec.productId == 9)
+                        select rec);
+            foreach (var row in data)
+            {
+                Console.WriteLine(row.ToString());
+                outputList.Add(row.ToString());
+            }
+        }
+        public void RetriveCounOfReviewForProductId(List<ProductReview> productReviewsList)
+        {
+            var records = (from rec in productReviewsList
+                           group rec by rec.productId into g
+                           select new
+                           {
+                               productId2 = g.Key,
+                               ReviewCount = g.Count()
+                           });
+            foreach (var row in records)
+            {
+                Console.WriteLine(row.ToString());
+                outputList.Add(row.ToString());
+            }
+        }
+        public void RetriveOnlyProductIdAndRating(List<ProductReview> productReviewsList)
+        {
+            var element = productReviewsList.GroupBy(x => x.productId).Select(x => new { productId = x.Key, Count = x.Count() });
+            foreach (var row in element)
+            {
+                Console.WriteLine(row.productId + "  " + row.Count);
+            }
+        }
+        public void SkipTopFiveRecord(List<ProductReview> productReviewsList)
+        {
+            var highestRatedRows = (from rec in productReviewsList
+                                    orderby rec.rating descending
+                                    select rec);
+            foreach (var row in highestRatedRows.Skip(5))
+            {
+                Console.WriteLine(row.ToString());
+                outputList.Add(row.ToString());
+            }
+        }
+    }
+}
---
+++ Linq/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Managment managment = new Managment();
+
+            /////////UC1
+            List<ProductReview> productReviewsList = new List<ProductReview>()
+            {
+            new ProductReview() { productId = 1, userId = 1, rating = 5, isLike = "Yes", review = "Awesome" },
+            new ProductReview() { productId = 1, userId = 2, rating = 3, isLike = "Yes", review = "Awesome" },
+            new ProductReview() { productId = 1, userId = 3, rating = 4, isLike = "Yes", review = "Awesome" },
+            new ProductReview() { productId = 1, userId = 4, rating = 3, isLike = "Yes", review = "Nice" },
+            new ProductReview() { productId = 1, userId = 5, rating = 4, isLike = "Yes", review = "Good" },
+            new ProductReview() { productId = 2, userId = 1, rating = 1, isLike = "No", review = "Unsatifactory" },
+            new ProductReview() { productId = 2, userId = 2, rating = 3, isLike = "Yes", review = "Good" },
+            new ProductReview() { productId = 2, userId = 3, rating = 4, isLike = "Yes", review = "Awesome" },
+            new ProductReview() { productId = 2, userId = 4, rating = 3, isLike = "Yes", review = "Nice" },
+            new ProductReview() { productId = 2, userId = 5, rating = 4, isLike = "Yes", review = "Good" },
+            new ProductReview() { productId = 3, userId = 1, rating = 5, isLike = "Yes", review = "Awesome" },
+            new ProductReview() { productId = 3, userId = 2, rating = 3, isLike = "Yes", review = "Awesome" },
+            new ProductReview() { productId = 3, userId = 3, rating = 4, isLike = "Yes", review = "Awesome" },
+            new ProductReview() { productId = 3, userId = 4, rating = 3, isLike = "Yes", review = "Nice" },
+            new ProductReview() { productId = 3, userId = 5, rating = 4, isLike = "Yes", review = "Good" },
+            new ProductReview() { productId = 4, userId = 1, rating = 1, isLike = "No", review = "Unsatifactory" },
+            new ProductReview() { productId = 4, userId = 2, rating = 2, isLike = "No", review = "Bad" },
+            new ProductReview() { productId = 4, userId = 3, rating = 2, isLike = "No", review = "Worst" },
+            new ProductReview() { productId = 4, userId = 4, rating = 3, isLike = "No", review = "Not good" },
+            new ProductReview() { productId = 4, userId = 5, rating = 2, isLike = "No", review = "Okay" },
+            new ProductReview() { productId = 5, userId = 1, rating = 5, isLike = "Yes", review = "Awesome" },
+            new ProductReview() { productId = 5, userId = 1, rating = 5, isLike = "Yes", review = "Awesome" },
+            new ProductReview() { productId = 5, userId = 1, rating = 5, isLike = "Yes", review = "Awesome" },
+            new ProductReview() { productId = 5, userId = 1, rating = 5, isLike = "Yes", review = "Awesome" },
+            new ProductReview() { productId = 5, userId = 1, rating = 5, isLike = "Yes", review = "Awesome" },
+            new ProductReview() { productId = 9, userId = 1, rating = 5, isLike = "Yes", review = "Does the work" },
+
+    };
+
+
+            /////////UC2
+            managment.RetriveTopThreeRecord(productReviewsList);
+
+            /////////UC3 
+            managment.RetriveAllRecordRatingGreaterThanThree(productReviewsList);
+
+            /////////UC4
+            managment.RetriveCounOfReviewForProductId(productReviewsList);
+
+            /////////UC5
+            managment.RetriveOnlyProductIdAndRating(productReviewsList);
+
+            /////////UC6
+            managment.SkipTopFiveRecord(productReviewsList);
+
+            Console.ReadKey();
+        }
+    }
+}
